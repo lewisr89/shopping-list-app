0421c9893c0c66f0b2a6156a35aea058b95e61e0
lewisr89 <lewisr89@gmail.com>
2014-03-01 18:54:40 -0500

    First Round

---
+++ application.js
@@ -0,0 +1,68 @@
+
+$(document).ready(function () {
+    
+    
+  $("#add-box").focus();
+
+  //Create variable for entering the item and appending it the unordered list
+    
+    var addItem = function () {
+        var currentItem = $("#add-box").val();
+        if (!currentItem || !currentItem.trim()) {
+            alert("You didn't enter anything."); 
+            }  
+            else {
+                $("#the-list").append("<li id='new-item'>" + currentItem + "<button class='remove-button'>X</button></li>");
+            }   
+        $("#add-box").val('');
+        $("#add-box").focus();
+    };
+
+    //Allows the enter key to be used for adding an item
+
+    $('#add-box').keypress(function (event) {
+        if (event.keyCode == 13) {
+            $("#add-button").click();
+           
+            event.preventDefault();
+        } 
+    });
+
+    //Makes the add button function when clicked
+    
+    $('#add-button').click(addItem);
+
+    //Make the 'x' button appear and disappear when hovering over a list item.
+
+    $("#the-list").on("mouseenter", "#new-item", function () {
+    $(this).find(".remove-button").show();
+  });
+
+    $("#the-list").on("mouseleave", "#new-item", function () {
+    $(this).find(".remove-button").hide();
+  });
+
+    //Toggle between checked off and not checked off
+
+    $("#the-list").on("click", "#new-item", function () {
+    $(this).toggleClass("checked");
+  });
+
+    //Remove items from the list
+
+    $("#the-list").on("click", ".remove-button", function () {
+    $(this).closest("li").remove();
+  });    
+
+   //Click the new list buttom and clear everything
+    var clear = function() {
+        $("#the-list").empty();
+        $("#add-box").focus();
+    };
+
+    $("#new-button").click(clear);
+
+
+
+});
+
